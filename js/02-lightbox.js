a1c35735f25200786c04015b98650dd9a74aaa85
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-07-14 21:38:21 +0300

    Task -2 Markup and added SimleLigthBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+// *1. Создание и рендер разметки по массиву данных galleryItems и предоставленному 
+// шаблону элемента галереи.Используй готовый код из первого задания.
+// *2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. 
+// Необходимо добавить ссылки на два файла: simple - lightbox.min.js и
+// simple - lightbox.min.css.
+// 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены 
+// в div.gallery.Для этого ознакомься с документацией SimpleLightbox - в первую 
+// очередь секции «Usage» и «Markup».
+// 4. Посмотри в документации секцию «Options» и добавь отображение подписей к 
+// изображениям из атрибута alt.Пусть подпись будет снизу и появляется через 
+// 250 миллисекунд после открытия изображения.
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.addEventListener('click', openOriginalImg);
+galleryItems.forEach(createGalleryItem);
+
+function createGalleryItem ({ preview, original, description }) {
+  galleryRef.insertAdjacentHTML('beforeend',
+    `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`); 
+}
+
+function openOriginalImg(event) {
+  event.preventDefault();
+  const selectedItemUrl = event.target.dataset.source;
+
+  var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+}
